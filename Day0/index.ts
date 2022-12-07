7f7a71118fdf14af882d88f0e7b92ed06a7ad8db
Mateusz Borycki <mateuszborycki@gmail.com>
2022-12-07 20:44:12 +0100

    Added manual version.

--- Day0/index.ts
+++ Day0/index.ts
@@ -4,9 +4,20 @@ import _ = require("lodash");
 let data = readFileSync('Day0/data.txt', 'utf8');
 let lines = data.split("\n");
 
-let sum = _.chain(lines)
+// 'manual' version
+let sum = 0;
+for (let line of lines)
+{
+   let number = Number(line);
+   sum += number;
+}
+
+console.log(sum);
+
+// 'lodash' version
+let l_sum = _.chain(lines)
             .map(line => Number(line))
             .sum()
             .value();
 
-console.log(sum);
+console.log(l_sum);
